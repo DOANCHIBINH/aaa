d98c5dfd079631791b5f724ffc0624c52ab64d23
DOANCHIBINH <97821942+DOANCHIBINH@users.noreply.github.com>
2022-10-23 13:32:36 +0700

    new app

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp3
+//
+//  Created by Huu Thanh on 23/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
